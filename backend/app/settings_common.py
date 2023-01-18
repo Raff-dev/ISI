5b362103d2a3fc407eb6c5d587194c0ed7089c71
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-01-18 19:58:19 +0100

    Docker compose tweaks

--- backend/app/settings_common.py
+++ backend/app/settings_common.py
@@ -8,7 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False")
+DEBUG = DEBUG.lower() == "true" or DEBUG is True
 
 ALLOWED_HOSTS = ["*"]
 
